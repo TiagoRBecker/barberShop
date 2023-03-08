32da719feb5ff9977ba0e59d6908b32e4988d29a
Tiago <beckertiago09@gmail.com>
2023-03-08 11:30:22 -0300

    producao

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head />
+      <Head >
+      <link rel="icon" href={"/icons.png"} type="image/x-icon" />
+      </Head>
       <body>
         <Main />
         <NextScript />
